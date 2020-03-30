84b3b96ee616b451eb3d2cb6d216506f015345b8
zikwall <andrey.kapitonov.96@gmail.com>
2020-03-30 16:43:14 +0300

    Add ui exports

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 import RePlayerMusic from './components/RePlayerMusic';
+import UISongItem from './components/ui/UISongItem';
+import UIAlbumItem from './components/ui/UIAlbumItem';
+import UIImage from './components/ui/UIImage';
+
+export {
+    UIAlbumItem, UIImage, UISongItem
+}
 
 export default RePlayerMusic;
