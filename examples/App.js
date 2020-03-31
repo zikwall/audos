d5d2dbdc5d191008753cf20d20a1dd4bda1a39f7
zikwall <andrey.kapitonov.96@gmail.com>
2020-03-31 16:46:41 +0300

    Updated examples

--- examples/App.js
+++ examples/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import AudosPlayer from 'audos';
-import UIPlaylisScene from 'audos/components/ui/UIPlaylistScene';
+import { UIPlaylisScene } from 'audos';
 
 import { playlist, actionItems } from './Data';
 
 const App = () => {
-    /*return <UIPlaylisScene
+    return <UIPlaylisScene
         colorScheme={'Dark'}
         items={playlist}
-    />*/
+    />
 
     return (
         <AudosPlayer
