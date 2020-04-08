b336154ac6b4b70859c31bbbfdbfe1fa34705298
zikwall <andrey.kapitonov.96@gmail.com>
2020-04-08 10:09:16 +0300

    Example app with react navigation v5

--- examples/App.js
+++ examples/App.js
@@ -1,23 +1,65 @@
 import React from 'react';
+import { View, ScrollView } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
 import AudosPlayer from 'audos';
 import { UIPlaylisScene } from 'audos';
-
 import { playlist, actionItems } from './Data';
 
-const App = () => {
-    return <UIPlaylisScene
-        colorScheme={'Dark'}
-        items={playlist}
-    />
+function PlayerScreen({ navigation }) {
+    navigation.setOptions({ tabBarVisible: false });
+
+    const navigateToUI = () => {
+        navigation.navigate('UI')
+    };
 
     return (
-        <AudosPlayer
-            colorScheme={'Dark'}
-            actionItems={actionItems}
-            items={playlist}
-            minimize={false}
-        />
-    )
-};
+        <View style={{ flex: 1 }}>
+            <AudosPlayer
+                colorScheme={'Dark'}
+                actionItems={actionItems}
+                items={playlist}
+                minimize={false}
+                onTopPress={navigateToUI}
+            />
+        </View>
+    );
+}
+
+function UIScreen() {
+    return (
+        <View style={{ flex: 1 }}>
+            <ScrollView>
+                <UIPlaylisScene
+                    colorScheme={'Dark'}
+                    items={playlist}
+                />
+            </ScrollView>
+        </View>
+    );
+}
+
+const Tab = createBottomTabNavigator();
+
+const App = () => (
+    <NavigationContainer>
+        <Tab.Navigator
+            tabBarOptions={{
+                activeTintColor: '#fff',
+                inactiveTintColor: '#ccc',
+                style:{
+                    backgroundColor: '#121212',
+                    borderTopColor: '#121212',
+                    paddingBottom: 15,
+                    paddingHorizontal: 5
+                }
+            }}
+        >
+            <Tab.Screen name="Player" component={PlayerScreen} />
+            <Tab.Screen name="UI" component={UIScreen} />
+        </Tab.Navigator>
+    </NavigationContainer>
+);
 
 export default App;
